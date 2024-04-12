4a00daad8a254a77b939c444e06be39b678c56d2
Sergey Stupak <itstupak@gmail.com>
2024-04-13 01:39:26 +0200

    finished task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,18 @@
 "use strict";
 
-
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
+    },
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    }
+};
 
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
